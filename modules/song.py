433c2b710d29baeeda1d1d891bec655eda35d9fb
brut✘⁶⁹ // ユスフ <87961324+brut-ctrl@users.noreply.github.com>
2021-08-31 04:43:40 +0700

    Add files via upload

--- modules/song.py
+++ modules/song.py
@@ -74,7 +74,7 @@ def song(client, message):
         m.edit("⛔ Found Nothing.\n\nTry another keywork or maybe spell it properly.")
         print(str(e))
         return
-    m.edit("Downloading the song ")
+    m.edit(" Downloading the song...")
     try:
         with youtube_dl.YoutubeDL(ydl_opts) as ydl:
             info_dict = ydl.extract_info(link, download=False)
