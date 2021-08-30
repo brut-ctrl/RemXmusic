d204c003c0274f2962f10510ba5ef9bd4c838cf9
brut✘⁶⁹ // ユスフ <87961324+brut-ctrl@users.noreply.github.com>
2021-08-31 02:09:54 +0700

    Add files via upload

--- helpers/filters.py
+++ helpers/filters.py
@@ -1,5 +1,5 @@
-# RemXmusic (Telegram bot project)
-# Copyright (C) 2021 @brut69
+# Calls Music 1 - Telegram bot for streaming audio in group calls
+# Copyright (C) 2021  Roj Serbest
 
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU Affero General Public License as
@@ -14,3 +14,18 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+
+from typing import List, Union
+
+from pyrogram import filters
+
+from config import COMMAND_PREFIXES
+
+other_filters = filters.group & ~filters.edited & ~filters.via_bot & ~filters.forwarded
+other_filters2 = (
+    filters.private & ~filters.edited & ~filters.via_bot & ~filters.forwarded
+)
+
+
+def command(commands: Union[str, List[str]]):
+    return filters.command(commands, COMMAND_PREFIXES)
