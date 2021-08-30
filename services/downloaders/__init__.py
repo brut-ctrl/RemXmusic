8bdf8526c9cc136101ab0854d649e893697d7d54
brut✘⁶⁹ // ユスフ <87961324+brut-ctrl@users.noreply.github.com>
2021-08-31 02:15:08 +0700

    Add files via upload

--- services/downloaders/__init__.py
+++ services/downloaders/__init__.py
@@ -13,3 +13,7 @@
 #
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+from services.downloaders import youtube
+
+__all__ = ["youtube"]
