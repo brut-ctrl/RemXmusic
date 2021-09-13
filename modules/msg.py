8ca996eb9302fd35215d80ca36a57c011c4993e8
brut✘⁶⁹ // ユスフ <87961324+brut-ctrl@users.noreply.github.com>
2021-09-14 04:25:49 +0700

    Add files via upload

--- modules/msg.py
+++ modules/msg.py
@@ -44,10 +44,9 @@ Click next for instructions**
 f"""
 **Setting up**
 
-1) Make bot admin (Group and in channel if use cplay)
+1) Make bot as admin (Group and in channel if use /cplay)
 2) Start a voice chat
-3) Try /play [song name] for the first time by an admin
-*) If userbot joined enjoy music, If not add @{ASSISTANT_NAME} to your group and retry
+3) Try /play <song title> for the first time by an @admin. If userbot joined enjoy music, If not add manualy @{ASSISTANT_NAME} to your group and report issue to Owner bot
 
 **For Channel Music Play**
 1) Make me admin of your channel 
@@ -58,21 +57,21 @@ f"""
 
 **✓ Song Playing 🎵**
 
-- /play: Play the requestd song
-- /play [yt url] : Play the given yt url
-- /play [reply yo audio]: Play replied audio
-- /splay: Play song via jio saavn
-- /ytplay: Directly play song via Youtube Music
+- /play - Play the requestd song
+- /play <yt url> - Play the given yt url
+- /play <reply audio file> - Play replied audio
+- /splay - Play song via jio saavn
+- /ytplay - Directly play song via Youtube Music
 
 **✓ Playback 🎵**
 
-- /player: Open Settings menu of player
-- /skip: Skips the current track
-- /pause: Pause track
-- /resume: Resumes the paused track
-- /end: Stops media playback
-- /current: Shows the current Playing track
-- /playlist: Shows playlist
+- /player - Open Settings menu of player
+- /skip - Skips the current track
+- /pause - Pause track
+- /resume - Resume the paused track
+- /end - Stops media playback
+- /current - Shows the current Playing track
+- /playlist - Shows playlist
 
 *Player cmd and all other cmds except /play, /current  and /playlist  are only for admins of the group.
 """,
@@ -82,44 +81,43 @@ f"""
 
 🥰 For linked group admins only
 
-- /cplay [song name] - play song you requested
-- /csplay [song name] - play song you requested via jio saavn
+- /cplay <song title> - Play song you requested
+- /csplay <song title> - Play song you requested via jio saavn
 - /cplaylist - Show now playing list
 - /cccurrent - Show now playing
-- /cplayer - open music player settings panel
-- /cpause - pause song play
-- /cresume - resume song play
-- /cskip - play next song
-- /cend - stop music play
-- /userbotjoinchannel - invite assistant to your chat
+- /cplayer - Open music player settings panel
+- /cpause - Pause song play
+- /cresume - Resume song play
+- /cskip - Play next song
+- /cend - Stop music play
+- /userbotjoinchannel - Invite assistant to your chat
 
-channel is also can be used instead of c ( /cplay = /channelplay )
+channel is also can be used instead of c /cplay or /channelplay
 
 🥰 If you donlt like to play in linked group
 
 1) Get your channel ID
-2) Create a group with tittle: Channel Music: your_channel_id
-3) Add bot as Channel admin with full perms
-4) Add @{ASSISTANT_NAME} to the channel as an admin
-5) Simply send commands in your group
+2) Add bot as Channel admin with full perms
+3) Add @{ASSISTANT_NAME} to the channel as an admin
+4) Simply send commands in your group
 """,
 
 f"""
 **✓ More tools ⚡**
 
-- /musicplayer [on/off] - Enable/Disable Music player
+- /musicplayer <on/off> - Enable/disable Music player
 - /admincache - Updates admin info of your group. Try if bot isn't recognize admin
 - /userbotjoin - Invite @{ASSISTANT_NAME} Userbot to your chat
-- /auth [reply to user] - Authorize User
-- /deauth [reply to user] - DeAuthorize user
+- /auth <reply to user> - Authorize User
+- /deauth <reply to user> - DeAuthorize user
 Authorized users can execute admin commands in authorized group
 
 **✓ Commands for Sudo Users ⚡**
 
- - /userbotleaveall - remove assistant from all chats
- - /gcast <reply to message> - globally brodcast replied message to all chats
- - /pmpermit [on/off] - enable/disable pmpermit message
-*Sudo Users can execute any command in any groups
+ - /userbotleaveall - Remove assistant from all chats
+ - /gcast <reply to message> - Globally brodcast replied message to all chats
+ - /pmpermit <on/off> - Enable/disable pmpermit message
+- Sudo Users can execute any command in any groups
 
 """
       ]
