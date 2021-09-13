bf8d95a8002fda4de94b9df2071bb824ef7a158c
brut✘⁶⁹ // ユスフ <87961324+brut-ctrl@users.noreply.github.com>
2021-09-14 03:50:57 +0700

    Add files via upload

--- services/callsmusic/__init__.py
+++ services/callsmusic/__init__.py
@@ -1,20 +1,6 @@
-# RemXmusic (Telegram bot project)
-# Copyright (C) 2021 @brut69
-
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License as
-# published by the Free Software Foundation, either version 3 of the
-# License, or (at your option) any later version.
-
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU Affero General Public License for more details.
-#
-# You should have received a copy of the GNU Affero General Public License
-# along with this program.  If not, see <https://www.gnu.org/licenses/>.
-
 from services.queues import queues
-from services.callsmusic.callsmusic import pytgcalls, run
+from pyrogram import Client
+import config
 
-__all__ = ["queues", "pytgcalls", "run"]
+client = Client(config.SESSION_NAME, config.API_ID, config.API_HASH)
+run = client.run
