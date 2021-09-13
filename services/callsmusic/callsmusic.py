7093f1dd4286f774c9645b21eba9553f4a688854
brut✘⁶⁹ // ユスフ <87961324+brut-ctrl@users.noreply.github.com>
2021-09-14 03:50:11 +0700

    Add files via upload

--- services/callsmusic/callsmusic.py
+++ services/callsmusic/callsmusic.py
@@ -15,25 +15,77 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 
-#from typing import Dict
-from pyrogram import Client
+from typing import Dict
+from import Client
 from pytgcalls import GroupCall
 
 from config import API_HASH, API_ID, SESSION_NAME
 from services.queues import queues
 
 client = Client(SESSION_NAME, API_ID, API_HASH)
-pytgcalls = GroupCall(client)
+#groupcall = GroupCall(client)
 
 
-@pytgcalls.on_stream_end()
-async def on_stream_end(client: GroupCall, chat_id: int) -> None:
-    queues.task_done(chat_id)
-
-    if queues.is_empty(chat_id):
-        await pytgcalls.leave_group_call(chat_id)
-    else:
-        await pytgcalls.change_stream(chat_id, queues.get(chat_id)["file"])
+instances: Dict[int, GroupCall] = {}
+active_chats: Dict[int, Dict[str, bool]] = {}
 
 
-run = pytgcalls.start
+def init_instance(chat_id: int):
+    if chat_id not in instances:
+        instances[chat_id] = GroupCall(client)
+
+    instance = instances[chat_id]
+
+    @instance.on_playout_ended
+    async def ___(__, _):
+        queues.task_done(chat_id)
+
+        if queues.is_empty(chat_id):
+            await stop(chat_id)
+        else:
+            instance.input_filename = queues.get(chat_id)["file"]
+
+
+def get_instance(chat_id: int) -> GroupCall:
+    init_instance(chat_id)
+    return instances[chat_id]
+
+
+async def start(chat_id: int):
+    await get_instance(chat_id).start(chat_id)
+    active_chats[chat_id] = {"playing": True}
+
+
+async def stop(chat_id: int):
+    await get_instance(chat_id).stop()
+
+    if chat_id in active_chats:
+        del active_chats[chat_id]
+
+
+async def set_stream(chat_id: int, file: str):
+    if chat_id not in active_chats:
+        await start(chat_id)
+    get_instance(chat_id).input_filename = file
+
+
+def pause(chat_id: int) -> bool:
+    if chat_id not in active_chats:
+        return False
+    elif not active_chats[chat_id]["playing"]:
+        return False
+
+    get_instance(chat_id).pause_playout()
+    active_chats[chat_id]["playing"] = False
+    return True
+
+
+def resume(chat_id: int) -> bool:
+    if chat_id not in active_chats:
+        return False
+    elif active_chats[chat_id]["playing"]:
+        return False
+
+    get_instance(chat_id).resume_playout()
+    active_chats[chat_id]["playing"] = True
+    return True
