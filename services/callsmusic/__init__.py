10889fe8b1cada66cbb9aee3a418542087b33777
brut✘⁶⁹ // ユスフ <87961324+brut-ctrl@users.noreply.github.com>
2021-10-25 03:53:49 +0700

    Update __init__.py

--- services/callsmusic/__init__.py
+++ services/callsmusic/__init__.py
@@ -1,4 +1,6 @@
-from services.queues import queues
-from services.callsmusic.callsmusic import groupcall, run
+from pyrogram import Client
+import config
 
-__all__ = ["queues", "groupcall", "run"]
+client = Client(config.SESSION_NAME, config.API_ID, config.API_HASH)
+run = client.run
+
