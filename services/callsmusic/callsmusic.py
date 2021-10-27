c7cb08b5dc7e03d060c1bdda9afa4fc28d4a4eb9
brut✘⁶⁹ // ユスフ <87961324+brut-ctrl@users.noreply.github.com>
2021-10-28 03:59:32 +0700

    Add files via upload

--- services/callsmusic/callsmusic.py
+++ services/callsmusic/callsmusic.py
@@ -19,8 +19,7 @@ from typing import Dict
 from pytgcalls import GroupCallFactory
 from config import API_HASH, API_ID, SESSION_NAME
 
-from .callsmusic import client
-from .callsmusic import group_call_instances
+from services.callsmusic.callsmusic import client
 from services.queues import queues
 from pyrogram import filters
 from pyrogram import Client
@@ -127,17 +126,4 @@ async def unmute(chat_id: int) -> int:
     return 0
 
 
-@client.on_message(filters.me & filters.command("start"))
-async def pl(__, _):
-    if _.chat.id in group_call_instances.active_chats:
-        queues.put(_.chat.id, 'out.raw')
-    else:
-        await group_call_instances.set_stream(_.chat.id, 'out.raw')
-
-
-@client.on_message(filters.me & filters.command("end"))
-async def pl(__, _):
-    await group_call_instances.stop(_.chat.id)
-
-client.run()
 run = client.run
