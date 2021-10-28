0f2b8cd3e82dbd573b4891692616eee5000f62c3
brut✘⁶⁹ // ユスフ <87961324+brut-ctrl@users.noreply.github.com>
2021-10-28 16:10:54 +0700

    Add files via upload

--- services/callsmusic/__init__.py
+++ services/callsmusic/__init__.py
@@ -1,8 +1,5 @@
 import config
-
 from pyrogram import Client
-from services.queues import queues
-from services.callsmusic.callsmusic import groupcall, run
 
 client = Client(config.SESSION_NAME, config.API_ID, config.API_HASH)
 run = client.run
