24bf950b82d9d5531220d56fcf3c921ab976f225
brut✘⁶⁹ // ユスフ <87961324+brut-ctrl@users.noreply.github.com>
2021-10-28 15:59:00 +0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
-import requests
 from pyrogram import Client as Bot
 
-from config import API_HASH, API_ID, BG_IMAGE, BOT_TOKEN
-from services.callsmusic.callsmusic import run
-
+from services.callsmusic import run
+from config import API_ID, API_HASH, BG_IMAGE, BOT_TOKEN, SUDO_USERS
+import requests
 
 response = requests.get(BG_IMAGE)
 file = open("./etc/foreground.png", "wb")
@@ -15,8 +14,7 @@ bot = Bot(
     API_ID,
     API_HASH,
     bot_token=BOT_TOKEN,
-    plugins=dict(root="modules"),
+    plugins=dict(root="modules")
 )
-
 bot.start()
 run()
